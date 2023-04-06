7ab48b1c35ab99595e3bf9736a9b3d3bc700f74a
Sanket Bhalerao <djsanketdibhalerao95@gmail.com>
2023-04-06 20:32:08 +0530

    user-dashboard created

--- smartcontactmanager/src/main/resources/static/js/script.js
+++ smartcontactmanager/src/main/resources/static/js/script.js
@@ -1,1 +1,29 @@
 console.log("This is my script file");
+
+// function toggleSidebar(){
+//     if($('.sidebar').is(":visible")){
+//         //true
+//         $(".sidebar").css("display","none");
+//         $(".content").css("margin-left","0%");
+//     }
+//     else{
+//         //false
+//         $(".sidebar").css("display","block");
+//         $(".content").css("margin-left","20%");
+//     }
+// };
+
+function toggleSidebar() {
+    
+    const sidebar =  document.getElementsByClassName("sidebar")[0];
+    const content =  document.getElementsByClassName("content")[0];
+
+    if(window.getComputedStyle(sidebar).display === "none"){
+        sidebar.style.display = "block";
+        content.style.marginLeft = "20%";
+    }
+    else{
+        sidebar.style.display = "none";
+        content.style.marginLeft = "0%";
+    }
+}
