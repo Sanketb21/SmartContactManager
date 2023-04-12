09f3df78b395c92c0618cad56781c0e915a7b79f
Sanket Bhalerao <djsanketdibhalerao95@gmail.com>
2023-04-12 13:08:17 +0530

    pagination implemented

--- smartcontactmanager/src/main/java/com/smart/dao/ContactRepository.java
+++ smartcontactmanager/src/main/java/com/smart/dao/ContactRepository.java
@@ -1,17 +1,19 @@
 package com.smart.dao;
 
-import java.util.ArrayList;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
 
-import antlr.collections.List;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 	//pagination
 	@Query("from Contact as c where c.user.id =:userId")
-	public ArrayList<Contact> findContactsByUser(@Param("userId") int userId);
+	public Page<Contact> findContactsByUser(@Param("userId") int userId, Pageable pageable);
 }
