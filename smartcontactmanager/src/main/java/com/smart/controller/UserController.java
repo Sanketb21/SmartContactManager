0367e599b201e59312d5b4119d0c667eab589933
Sanket Bhalerao <djsanketdibhalerao95@gmail.com>
2023-04-14 14:19:01 +0530

    profile page built

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -250,5 +250,13 @@ public class UserController {
 		return "redirect:/user/" + contact.getcId()+"/contact";
 	}
 	
+	//your profile handler
+	@GetMapping("/profile")
+	public String yourProfile(Model model) {
+		
+		model.addAttribute("title","Profile Page");
+		return "normal/profile";
+	}
+	
 	
 }
